c0563b05cb812766afa905cb61c706e59c891456
Marion Jhon Agapay <marionagapay2001@gmail.com>
2026-01-09 16:31:27 +0800

    fix: remove the  console.log

--- app/components/ProductChart.tsx
+++ app/components/ProductChart.tsx
@@ -15,7 +15,6 @@ export interface ChartData {
 }
 
 const ProductChart = ({ data }: { data: ChartData[] }) => {
-  console.log(data);
   return (
     <div className="h-48 w-full">
       <ResponsiveContainer width="100%" height="100%">
